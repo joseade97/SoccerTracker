fdd4e01f1f909a4857e275eebb8dd4d6120b242e
amora95 <albertoamp95@gmail.com>
2020-04-17 21:57:15 -0600

    Validación de Persona y crifrado de contraseña

--- Proyecto/Formularios/frmIngresarUsuario.aspx
+++ Proyecto/Formularios/frmIngresarUsuario.aspx
@@ -3,6 +3,7 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
 
     <form id="frm1" runat="server" class="text-center">
+        <asp:HiddenField runat="server" ID="hdfIdPersona"/>
         <h2>Ingresar Nuevo Usuario</h2>
         <label class="mb-1">Nombre de Usuario</label>
         <br />
@@ -18,22 +19,26 @@
         <asp:RequiredFieldValidator CssClass="validator-message" ID="RequiredFieldValidator5" runat="server" ErrorMessage="Este campo es requerido" ControlToValidate="ddlTipoUsu"></asp:RequiredFieldValidator>
         <asp:DropDownList runat="server" ID="ddlTipoUsu">
             <asp:ListItem Text="Seleccione" Value="" />
-            <asp:ListItem Text="Administrador" Value="1" />
-            <asp:ListItem Text="Genérico" Value="0" />
+            <asp:ListItem Text="Administrador" Value="true" />
+            <asp:ListItem Text="Genérico" Value="false" />
         </asp:DropDownList>
         <br />
         <label class="mb-1">Estado del Usuario</label>
         <asp:RequiredFieldValidator CssClass="validator-message" ID="RequiredFieldValidator3" runat="server" ErrorMessage="Este campo es requerido" ControlToValidate="ddlEstado"></asp:RequiredFieldValidator>
         <asp:DropDownList runat="server" ID="ddlEstado">
             <asp:ListItem Text="Seleccione" Value="" />
-            <asp:ListItem Text="Activo" Value="1" />
-            <asp:ListItem Text="Inactivo" Value="0" />
+            <asp:ListItem Text="Activo" Value="true" />
+            <asp:ListItem Text="Inactivo" Value="false" />
         </asp:DropDownList>
         <br />
-        <label class="mb-1">Nombre de Persona</label>
+        <label class="mb-1">Número de identificación</label>
         <br />
-        <asp:RequiredFieldValidator CssClass="validator-message" ID="RequiredFieldValidator4" runat="server" ErrorMessage="Este campo es requerido" ControlToValidate="txtNombrePersona" ForeColor="#FF5050"></asp:RequiredFieldValidator>
-        <asp:TextBox runat="server" type="text" ID="txtNombrePersona" class="form-control md-textbox btn" BorderColor="Black" BorderStyle="Solid"></asp:TextBox>
+        <asp:RequiredFieldValidator CssClass="validator-message" ID="RequiredFieldValidator4" runat="server" ErrorMessage="Este campo es requerido" ControlToValidate="txtIdentificacion" ForeColor="#FF5050"></asp:RequiredFieldValidator>
+        <asp:TextBox runat="server" type="text" ID="txtIdentificacion" class="form-control md-textbox btn" BorderColor="Black" BorderStyle="Solid"></asp:TextBox> <asp:Button Text="Validar identificación" ID="btnVal" class="btn btn-alter btn-s" runat="server" OnClick="btnVal_Click"  />
+        <br />
+        <label class="mb-1">Nombre de Persona Seleccionada</label>
+        <br />
+        <asp:TextBox runat="server" disabled="disabled" ID="txtNombre"/>
         <br />
         <asp:Button Text="Guardar" ID="btnSubmit" class="btn btn-alter btn-xl" runat="server" OnClick="btnSubmit_Click" />
         <asp:ScriptManager ID="ScriptManager1" runat="server">
--- Proyecto/Formularios/frmIngresarUsuario.aspx.cs
+++ Proyecto/Formularios/frmIngresarUsuario.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 using Proyecto.Modelos;
 using Microsoft.Ajax;
 using Proyecto.Clases;
-
+using Liphsoft.Crypto.Argon2;
 
 namespace Proyecto.Formularios
 {
@@ -16,73 +16,41 @@ namespace Proyecto.Formularios
         ProyectoBD modelo = new ProyectoBD();
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                var tipoUsu = (from u in modelo.Usuarios
-                                  select u).ToList();
-                ddlTipoUsu.DataSource = tipoUsu;
-                ddlTipoUsu.DataBind();
-                ddlTipoUsu.DataTextField = "es_admin";
-                ddlTipoUsu.DataValueField = "es_admin";
-                ddlTipoUsu.DataBind();
-                ddlTipoUsu.Items.Insert(0, new ListItem("Seleccione", ""));
 
-                var estadoUsu = (from u in modelo.Usuarios
-                                  select u).ToList();
-                ddlEstado.DataSource = estadoUsu;
-                ddlEstado.DataBind();
-                ddlEstado.DataTextField = "estado";
-                ddlEstado.DataValueField = "estado";
-                ddlEstado.DataBind();
-                ddlEstado.Items.Insert(0, new ListItem("Seleccione", ""));
-            }
         }
 
         protected void ddlTipoUsu_SelectedIndexChanged(object sender, EventArgs e)
         {
-            var es_admin = Boolean.Parse(ddlTipoUsu.SelectedValue);
-            var tipoUsu = (from u in modelo.Usuarios
-                           where u.es_admin == es_admin
-                           select u).ToList();
-            ddlTipoUsu.DataSource = tipoUsu;
-            ddlTipoUsu.DataBind();
-            ddlTipoUsu.DataTextField = "es_admin";
-            ddlTipoUsu.DataValueField = "es_admin";
-            ddlTipoUsu.DataBind();
-            ddlTipoUsu.Items.Insert(0, new ListItem("Seleccione", ""));
+
         }
         protected void ddlEstado_SelectedIndexChanged(object sender, EventArgs e)
         {
-            var estado = Boolean.Parse(ddlEstado.SelectedValue);
-            var estadoUsu = (from u in modelo.Usuarios
-                             where u.estado == estado
-                             select u).ToList();
-
-            ddlEstado.DataSource = estadoUsu;
-            ddlEstado.DataBind();
-            ddlEstado.DataTextField = "estado";
-            ddlEstado.DataValueField = "estado";
-            ddlEstado.DataBind();
-            ddlEstado.Items.Insert(0, new ListItem("Seleccione", ""));
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
             if (this.IsValid)
             {
-                try
+                if (!String.IsNullOrEmpty(hdfIdPersona.Value))
                 {
-                    Usuario nUsuario = GenerarNuevoUsuario();
-                    modelo.Usuarios.Add(nUsuario);
-                    modelo.SaveChanges();
-                    Utilidades.CreateMessageByScript(ClientScript, GetType(), "El usuario ha sido registrado con éxito");
-                    Utilidades.ClearTextBoxes(this);
+                    try
+                    {
+                        Usuario nUsuario = GenerarNuevoUsuario();
+                        modelo.Usuarios.Add(nUsuario);
+                        modelo.SaveChanges();
+                        Utilidades.CreateMessageByScript(ClientScript, GetType(), "El usuario ha sido registrado con éxito.");
+                        Utilidades.ClearTextBoxes(this);
+                    }
+                    catch (Exception ex)
+                    {
+                        string msg = "Ha ocurrido un error inesperado, por favor comuníquese con" +
+                                            " el administrador de la web brindándole la siguiente información: " + ex.Message;
+                        Utilidades.CreateMessageByScript(ClientScript, GetType(), msg);
+                    }
                 }
-                catch (Exception ex)
+                else
                 {
-                    string msg = "Ha ocurrido un error inesperado, por favor comuníquese con" +
-                                        " el administrador de la web brindándole la siguiente información: " + ex.Message;
-                    Utilidades.CreateMessageByScript(ClientScript, GetType(), msg);
+                    Utilidades.CreateMessageByScript(ClientScript, GetType(), "Por favor valide el número de identificación para continuar.");
                 }
 
             }
@@ -90,14 +58,26 @@ namespace Proyecto.Formularios
 
         protected Usuario GenerarNuevoUsuario()
         {
-            Usuario nUsuario = new Usuario();
+            var hasher = new PasswordHasher();
+            string hashed_pass = hasher.Hash(txtPassword.Text);
 
+            Usuario nUsuario = new Usuario();
+            
             nUsuario.nombre_usuario = txtNombreUsuario.Text;
-            nUsuario.hashed_pass = txtPassword.Text;
+            nUsuario.hashed_pass = hashed_pass;
             nUsuario.es_admin = Boolean.Parse(ddlTipoUsu.SelectedValue);
             nUsuario.estado = Boolean.Parse(ddlEstado.SelectedValue);
-            nUsuario.id_persona = Int32.Parse(txtNombrePersona.Text);
+            nUsuario.id_persona = Int32.Parse(hdfIdPersona.Value);
             return nUsuario;
+        }
+
+        protected void btnVal_Click(object sender, EventArgs e)
+        {
+            var persona = (from u in modelo.Personas
+                           where u.num_identificacion == txtIdentificacion.Text
+                           select new { u.id, nombre = u.nombre + " " + u.ape1 + " " + u.ape2 }).FirstOrDefault();
+            hdfIdPersona.Value = persona.id.ToString();
+            txtNombre.Text = persona.nombre;
         }
     }
 }
